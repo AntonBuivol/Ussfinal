0351da02b5bfdf4f61fb35f0602a59de2a83089f
AntonBuivol <120181261+AntonBuivol@users.noreply.github.com>
2023-05-24 19:57:49 +0300

    Add files via upload

---
+++ Store.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uss
+{
+    internal class Store
+    {
+        Dictionary<string, User> newusers = new Dictionary<string, User>();
+
+        string filePath = @"..\..\..\Users.txt";
+        public void AddUser(User user)
+        {
+            if (newusers.ContainsKey(user.username))
+            {
+                newusers[user.username] = user;
+            }
+            else
+            {
+                newusers.Add(user.username, user);
+            }
+            
+        }
+        public void save()
+        {
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                StreamWriter login = new StreamWriter(filePath, true);
+                foreach (KeyValuePair<string, User> keyValue in newusers)
+                {
+                    string user = $"{keyValue.Key} {keyValue.Value.score}";
+                    login.WriteLine(user);
+                }
+                login.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Viga failiga " + ex.Message);
+            }
+
+        }
+        public void read()
+        {
+            try
+            {
+                StreamReader logins = new StreamReader(filePath, true);
+                string line;
+                while ((line = logins.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(' ');
+
+                    string username = parts[0]; // Имя пользователя
+                    int score = int.Parse(parts[1]); // Преобразование счета в целое число
+
+                    //Console.WriteLine(line);
+                    AddUser(new User(username, score));
+                }
+                logins.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Viga failiga " + ex.Message);
+            }
+        }
+    }
+    
+}
