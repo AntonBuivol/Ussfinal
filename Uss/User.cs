4624dfb554b8610bd259d99b767d014be2ffd7f4
AntonBuivol <120181261+AntonBuivol@users.noreply.github.com>
2023-05-28 17:30:31 +0300

    Update User.cs

--- Uss/User.cs
+++ Uss/User.cs
@@ -12,10 +12,10 @@ namespace Uss
         public int score { get; set; }
         public User(string username) 
         {
-            this.username = username;
-            score = 0;
+            this.username = username; //name это переменная метода которая передает значение в username, this (username) для того чтобы отличать переменные объекта от переменных метода (name)
+            score = 0; //нет this так как метода нет переменной с таким же иминем
         }
-        public User(string username,int score)
+        public User(string username,int score) //для обновления
         {
             this.username = username;
             this.score = score;
